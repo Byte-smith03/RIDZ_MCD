6463e6c6103e0115e57f60a922ee1c0a10a34cb9
Ridz Coder <smtechofcmods@gmail.com>
2025-08-10 19:12:32 +0300

    Create a new account

---
+++ ridzplugs/menu.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+let trashplug = async (m, {trashcore,replymenu,menu}) => {
+replymenu(`${menu}
+`)
+};
+trashplug.help = ['ridzmcd']
+trashplug.tags = ['menu']
+trashplug.command = ['menu']
+
+
+module.exports = trashplug;
