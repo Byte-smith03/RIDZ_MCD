9eba2402638980a06c9e3dd4494b763582e76ade
Ridz Coder <smtechofcmods@gmail.com>
2025-08-10 16:40:15 +0300

    Create ridzenv.js

---
+++ ridzenv.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+const chalk = require('chalk')
+if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
+
+
+global.SESSION_ID = process.env.SESSION_ID || '' 
+// Owner Setting
+global.xprefix = process.env.BOT_PREFIX ||'.'
+global.owner = ["256742634089",]
+global.error = ["6666",]
+global.ownername = process.env.OWNER_NAME ||'Ridz Coder'
+global.antidelete = process.env.ANTI_DELETE || true
+//━━━━━━━━━━━━━━━━━━━━━━━━//
+// Bot Setting
+global.botname = "𝑹𝑰𝑫𝒁 𝑴𝑪𝑫"
+global.botversion = "1.6.0"
+global.typebot = "Plugin"
+global.session = "ridzsession"
+global.connect = true
+global.statusview = process.env.AUTO_STATUS || true
+global.antilinkgc = process.env.ANTILINK_GROUP || true
+global.autoTyping = process.env.AUTO_TYPE || false
+global.autoRecord = process.env.AUTO_RECORD || false
+global.thumb = "https://files.catbox.moe/4ryp6k.jpg"
+global.wagc = "https://whatsapp.com/channel/0029VbB5hQD9mrGjnplDZp2l"
+global.caption = "Cʀᴇᴀᴛᴇᴅ ʙʏ Rɪᴅᴢ Cᴏᴅᴇʀ"
+//━━━━━━━━━━━━━━━━━━━━━━━━//
+// Sticker Marker
+global.packname = process.env.PACK_NAME ||'𝑹𝑰𝑫𝒁 𝑴𝑪𝑫'
+global.packname = process.env.AUTHOR||'𝐏𝐀𝐂𝐊𝐒'
+//━━━━━━━━━━━━━━━━━━━━━━━━//
+// Respon Message
+global.mess = {
+    success: '✅ Done.',
+    admin: '🚨 Admin only.',
+    premium: '🆘must be a premium user.',
+    botAdmin: '🤖 Make me admin first.',
+    owner: '👑 Owner only.',
+    OnlyGrup: '👥 Group only.',
+    private: '📩 Private chat only.',
+    wait: '⏳ Processing...',
+    error: '⚠️ Error occurred.',
+}
+//━━━━━━━━━━━━━━━━━━━━━━━━//
+// File Update
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(`Update File 📁 : ${__filename}`)
+delete require.cache[file]
+require(file)
+})
